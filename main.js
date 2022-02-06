3870488d1ca0eba815c7d7e507422badea65d592
Alex Jordan <AlJord91@gmail.com>
2022-02-05 20:49:51 -0500

    buttons ~working

--- main.js
+++ main.js
@@ -1,9 +1,83 @@
-let buttOne = document.getElementById('buttOne')
-console.log(buttOne)
-console.log(buttOne.innerHTML)
+// id the display
+// id all buttons add event listeners
+// when buttons are clicked, edit innertext of display to equal innertext of buttons
+// when equals is clicked use eval to evaluate sting in innertext of display
 
-buttOne.addEventListener('click', function() {
-    console.log('Box' + buttOne.innerHTML + ' was clicked')
+// let buttOne = document.getElementById('buttOne')
+// console.log(buttOne)
+// console.log(buttOne.innerHTML)
+
+// buttOne.addEventListener('click', function() {
+//     console.log('Box' + buttOne.innerHTML + ' was clicked')
     
 
+// })
+
+calc.addEventListener('click', function (event){
+    console.log(event.target.innerHTML)
+    
 })
+
+
+
+
+
+
+
+
+
+
+
+// buttSeven.addEventListener('click', function() {console.log(buttSeven.innerHTML); });
+
+// buttEight.addEventListener('click', function() {console.log(buttEight.innerHTML); });
+
+// buttNine.addEventListener('click', function() {console.log(buttNine.innerHTML); });
+
+// buttFour.addEventListener('click', function() {console.log(buttFour.innerHTML); });
+
+// buttFive.addEventListener('click', function() {console.log(buttFive.innerHTML); });
+
+// buttSix.addEventListener('click', function() {console.log(buttSix.innerHTML); });
+
+// buttOne.addEventListener('click', function() {console.log(buttOne.innerHTML); });
+
+// buttTwo.addEventListener('click', function() {console.log(buttTwo.innerHTML); });
+
+// buttThree.addEventListener('click', function() {console.log(buttThree.innerHTML); });
+
+// buttZero.addEventListener('click', function() {console.log(buttZero.innerHTML); });
+
+
+
+// buttClr.addEventListener('click', function() {console.log(buttClr.innerHTML); });
+// buttDivide.addEventListener('click', function() {console.log(buttDivide.innerHTML); });
+// buttMulti.addEventListener('click', function() {console.log(buttMulti.innerHTML); });
+// buttMinus.addEventListener('click', function() {console.log(buttMinus.innerHTML); });
+// buttDecimel.addEventListener('click', function() {console.log(buttDecimel.innerHTML); });
+// buttEquals.addEventListener('click', function() {console.log(buttEquals.innerHTML); });
+// buttPlus.addEventListener('click', function() {console.log(buttPlus.innerHTML); });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
