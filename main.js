af0bbd1b384f6fea2cda49f4473d92ad49229c7d
Alex Jordan <AlJord91@gmail.com>
2022-02-06 18:05:52 -0500

    one working calculator

--- main.js
+++ main.js
@@ -3,69 +3,94 @@
 // when buttons are clicked, edit innertext of display to equal innertext of buttons
 // when equals is clicked use eval to evaluate sting in innertext of display
 
-// let buttOne = document.getElementById('buttOne')
-// console.log(buttOne)
-// console.log(buttOne.innerHTML)
 
-// buttOne.addEventListener('click', function() {
-//     console.log('Box' + buttOne.innerHTML + ' was clicked')
+
+
+let displayBox=document.getElementById('buttResult');
+displayBox.innerHTML= ''
+console.log(displayBox)
+
+buttSeven.addEventListener('click', function() { 
+    (displayBox.innerHTML= displayBox.innerHTML+7);
+    console.log(displayBox.innerHTML)})
+    
     
 
-// })
+buttEight.addEventListener('click', function() { 
+    (displayBox.innerHTML= displayBox.innerHTML+8);
+    console.log(displayBox.innerHTML)})
 
-calc.addEventListener('click', function (event){
-    console.log(event.target.innerHTML)
-    
-})
+buttNine.addEventListener('click', function() { 
+    (displayBox.innerHTML= displayBox.innerHTML+9);
+    console.log(displayBox.innerHTML)})
 
+buttFour.addEventListener('click', function() { 
+    (displayBox.innerHTML= displayBox.innerHTML+4);
+    console.log(displayBox.innerHTML)})
 
+buttFive.addEventListener('click', function() { 
+    (displayBox.innerHTML= displayBox.innerHTML+5);
+    console.log(displayBox.innerHTML)})
 
+buttSix.addEventListener('click', function() { 
+    (displayBox.innerHTML= displayBox.innerHTML+6);
+    console.log(displayBox.innerHTML)})
 
+buttOne.addEventListener('click', function() { 
+    (displayBox.innerHTML= displayBox.innerHTML+1);
+    console.log(displayBox.innerHTML)})
 
+buttTwo.addEventListener('click', function() { 
+    (displayBox.innerHTML= displayBox.innerHTML+2);
+    console.log(displayBox.innerHTML)})
 
+buttThree.addEventListener('click', function() { 
+    (displayBox.innerHTML= displayBox.innerHTML+3);
+    console.log(displayBox.innerHTML)})
 
-
-
-
-
-// buttSeven.addEventListener('click', function() {console.log(buttSeven.innerHTML); });
-
-// buttEight.addEventListener('click', function() {console.log(buttEight.innerHTML); });
-
-// buttNine.addEventListener('click', function() {console.log(buttNine.innerHTML); });
-
-// buttFour.addEventListener('click', function() {console.log(buttFour.innerHTML); });
-
-// buttFive.addEventListener('click', function() {console.log(buttFive.innerHTML); });
-
-// buttSix.addEventListener('click', function() {console.log(buttSix.innerHTML); });
-
-// buttOne.addEventListener('click', function() {console.log(buttOne.innerHTML); });
-
-// buttTwo.addEventListener('click', function() {console.log(buttTwo.innerHTML); });
-
-// buttThree.addEventListener('click', function() {console.log(buttThree.innerHTML); });
-
-// buttZero.addEventListener('click', function() {console.log(buttZero.innerHTML); });
-
-
-
-// buttClr.addEventListener('click', function() {console.log(buttClr.innerHTML); });
-// buttDivide.addEventListener('click', function() {console.log(buttDivide.innerHTML); });
-// buttMulti.addEventListener('click', function() {console.log(buttMulti.innerHTML); });
-// buttMinus.addEventListener('click', function() {console.log(buttMinus.innerHTML); });
-// buttDecimel.addEventListener('click', function() {console.log(buttDecimel.innerHTML); });
-// buttEquals.addEventListener('click', function() {console.log(buttEquals.innerHTML); });
-// buttPlus.addEventListener('click', function() {console.log(buttPlus.innerHTML); });
+buttZero.addEventListener('click', function() { 
+    (displayBox.innerHTML= displayBox.innerHTML+0);
+    console.log(displayBox.innerHTML)})
 
 
 
 
 
+buttClr.addEventListener('click', function() { 
+    (displayBox.innerHTML= '' );
+    console.log(displayBox.innerHTML)})
+
+buttDivide.addEventListener('click', function() { 
+    (displayBox.innerHTML= displayBox.innerHTML+divide)
+    console.log(displayBox.innerHTML)})
+
+buttMulti.addEventListener('click', function() { 
+    (displayBox.innerHTML= displayBox.innerHTML+multi);
+    console.log(displayBox.innerHTML)})
+
+buttMinus.addEventListener('click', function() { 
+    (displayBox.innerHTML= displayBox.innerHTML+minus);
+    console.log(displayBox.innerHTML)})
+
+buttDecimel.addEventListener('click', function() { 
+    (displayBox.innerHTML= displayBox.innerHTML+decimel);
+    console.log(displayBox.innerHTML)})
+
+buttEquals.addEventListener('click', function() {; 
+    displayBox.innerHTML=eval(displayBox.innerHTML)
+    console.log(eval(displayBox.innerHTML))});
+
+buttPlus.addEventListener('click', function() { 
+    (displayBox.innerHTML= displayBox.innerHTML+plus);
+    console.log(displayBox.innerHTML)})
 
 
 
-
+let decimel='.'
+let minus ='-'
+let multi = '*'
+let divide= '/'
+let plus='+'
 
 
 
